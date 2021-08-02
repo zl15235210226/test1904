fe2a9a7f82f9b0194e765db84ec7f0aa527e970f
zl15235210226 <85332564+zl15235210226@users.noreply.github.com>
2021-08-02 22:05:52 +0800

    123

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         private String id;
         private String name;
+        private String address;
         System.out.println();
     }
 }
